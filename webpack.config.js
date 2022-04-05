d05d8d3503d8a376923619de8deb11ef227f5361
Masoudi Parrot <masoudi@localhost.localdomain>
2022-04-05 14:56:21 -0400

    Implement the UI

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
    mode: 'development',
    entry: {
      index: './src/index.js',
-     print: './src/print.js',
    },
    devtool: 'inline-source-map',
 
